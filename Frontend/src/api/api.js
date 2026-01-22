18e1d5efc3d93aae11d7a01698cbae4c77939cf3
Rahul Kamat <kamatrahul415@gmail.com>
2026-01-22 12:29:19 +0530

    Final Commit

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://shopaura-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json'
   }
   // http://localhost:5000
   // https://shop-aura.onrender.com
+  //https://shopaura-production.up.railway.app/
 });
 
 export default api;
